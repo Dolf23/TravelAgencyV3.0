3173d5a7540fee7dc6034c9ce9858b25b96f0bbb
Dolf <dmitry.dailidovich@gmail.com>
2016-05-26 19:39:51 +0300

    ListenEntityFilter delete

--- web/src/main/java/by/it_academy/TravelAgency/commands/admin/GoToCreateTourCommand.java
+++ web/src/main/java/by/it_academy/TravelAgency/commands/admin/GoToCreateTourCommand.java
@@ -2,14 +2,36 @@ package by.it_academy.TravelAgency.commands.admin;
 
 import by.it_academy.TravelAgency.commands.AbstractCommand;
 import by.it_academy.TravelAgency.constants.ConfigsConstants;
+import by.it_academy.TravelAgency.constants.Parameters;
+import by.it_academy.TravelAgency.dao.*;
+import by.it_academy.TravelAgency.dto.*;
+import by.it_academy.TravelAgency.logger.logger;
 import by.it_academy.TravelAgency.managers.ConfigurationManager;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.List;
 
 public class GoToCreateTourCommand extends AbstractCommand {
     @Override
     public String execute(HttpServletRequest request) {
         String page = ConfigurationManager.INSTANCE.getProperty(ConfigsConstants.ADMIN_CREATE_TOUR_PAGE_PATH);
+
+        try {
+            List<TourType> typeTourList = TourTypeDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.TOUR_TYPE_LIST, typeTourList);
+
+            List<Transport> transportList = TransportDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.TRANSPORT_LIST, transportList);
+
+            List<HotelType> hotelList = HotelTypeDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.HOTEL_TYPE_LIST, hotelList);
+
+            List<FoodComplex> foodComplexList = FoodComplexDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.FOOD_COMPLEX_LIST, foodComplexList);
+        } catch (SQLException e) {
+            logger.writeLog(e.getMessage());
+        }
         return page;
     }
 }
--- web/src/main/java/by/it_academy/TravelAgency/commands/user/GoToSelectTourCommand.java
+++ web/src/main/java/by/it_academy/TravelAgency/commands/user/GoToSelectTourCommand.java
@@ -2,14 +2,39 @@ package by.it_academy.TravelAgency.commands.user;
 
 import by.it_academy.TravelAgency.commands.AbstractCommand;
 import by.it_academy.TravelAgency.constants.ConfigsConstants;
+import by.it_academy.TravelAgency.constants.Parameters;
+import by.it_academy.TravelAgency.dao.*;
+import by.it_academy.TravelAgency.dto.*;
+import by.it_academy.TravelAgency.logger.logger;
 import by.it_academy.TravelAgency.managers.ConfigurationManager;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
+import java.util.List;
 
 public class GoToSelectTourCommand extends AbstractCommand {
     @Override
     public String execute(HttpServletRequest request) {
         String page = ConfigurationManager.INSTANCE.getProperty(ConfigsConstants.USER_SELECT_TOUR_PAGE_PATH);
+
+        try {
+            List<TourType> typeTourList = TourTypeDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.TOUR_TYPE_LIST, typeTourList);
+
+            List<Country> countryList = CountryDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.COUNTRY_LIST, countryList);
+
+            List<Transport> transportList = TransportDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.TRANSPORT_LIST, transportList);
+
+            List<HotelType> hotelList = HotelTypeDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.HOTEL_TYPE_LIST, hotelList);
+
+            List<FoodComplex> foodComplexList = FoodComplexDAO.INSTANCE.getAll();
+            request.setAttribute(Parameters.FOOD_COMPLEX_LIST, foodComplexList);
+        } catch (SQLException e) {
+            logger.writeLog(e.getMessage());
+        }
         return page;
     }
 }
