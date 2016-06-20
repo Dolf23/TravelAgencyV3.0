800b5c74afbc131f69fea064f52ccb7a42c1ce42
Dolf <dmitry.dailidovich@gmail.com>
2016-06-21 00:11:00 +0300

    make reservation complete

--- dao/src/main/java/by/it_academy/agency/dao/ActionDAO.java
+++ dao/src/main/java/by/it_academy/agency/dao/ActionDAO.java
@@ -3,13 +3,13 @@ package by.it_academy.agency.dao;
 import by.it_academy.agency.beans.Action;
 import by.it_academy.agency.beans.User;
 import by.it_academy.agency.constants.ColumnNames;
-import by.it_academy.agency.constants.SQLRequests;
 import by.it_academy.agency.exceptions.DAOException;
 import by.it_academy.agency.logger.logger;
 import by.it_academy.agency.utils.HibernateUtil;
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import java.util.List;
 
@@ -17,9 +17,9 @@ public class ActionDAO extends AbstractDAO<Action> {
     public List<Action> getListUserActions(User user) throws DAOException {
         try {
             Session session = HibernateUtil.getSession();
-            Query query = session.createQuery(SQLRequests.GET_ACTION_BY_USER);
-            query.setParameter(ColumnNames.ACTIONS_FK_USER, user.getId());
-            List<Action> list = query.list();
+            Criteria criteria = session.createCriteria(Action.class);
+            criteria.add(Restrictions.eq(ColumnNames.ACTIONS_FK_USER + "." + ColumnNames.USERS_ID, user.getId()));
+            List<Action> list = criteria.list();
             return list;
         } catch (HibernateException e) {
             logger.writeLog("ActionDAO getListUserActions error:" + e.getMessage());
--- dao/src/main/java/by/it_academy/agency/dao/TourDAO.java
+++ dao/src/main/java/by/it_academy/agency/dao/TourDAO.java
@@ -2,13 +2,13 @@ package by.it_academy.agency.dao;
 
 import by.it_academy.agency.beans.Tour;
 import by.it_academy.agency.constants.ColumnNames;
-import by.it_academy.agency.constants.SQLRequests;
 import by.it_academy.agency.exceptions.DAOException;
 import by.it_academy.agency.logger.logger;
 import by.it_academy.agency.utils.HibernateUtil;
+import org.hibernate.Criteria;
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import java.util.List;
 
@@ -27,13 +27,13 @@ public class TourDAO extends AbstractDAO<Tour> {
     public List<Tour> getToursByRequest(int tourType, int country, int transport, int hotelType, int foodComplex) throws DAOException {
         try {
             Session session = HibernateUtil.getSession();
-            Query query = session.createQuery(SQLRequests.GET_TOURS_BY_REQUEST);
-            query.setParameter(ColumnNames.TOURS_FK_TOUR_TYPE, tourType);
-            query.setParameter(ColumnNames.TOURS_FK_COUNTRY, country);
-            query.setParameter(ColumnNames.TOURS_FK_TRANSPORT, transport);
-            query.setParameter(ColumnNames.TOURS_FK_HOTEL_TYPE, hotelType);
-            query.setParameter(ColumnNames.TOURS_FK_FOOD_COMPLEX, foodComplex);
-            List<Tour> list = query.list();
+            Criteria criteria = session.createCriteria(Tour.class);
+            criteria.add(Restrictions.eq(ColumnNames.TOURS_FK_TOUR_TYPE + "." + ColumnNames.TOUR_TYPES_ID, tourType));
+            criteria.add(Restrictions.eq(ColumnNames.TOURS_FK_COUNTRY + "." + ColumnNames.COUNTRIES_ID, country));
+            criteria.add(Restrictions.eq(ColumnNames.TOURS_FK_TRANSPORT + "." + ColumnNames.TRANSPORTS_ID, transport));
+            criteria.add(Restrictions.eq(ColumnNames.TOURS_FK_HOTEL_TYPE + "." + ColumnNames.HOTEL_TYPES_ID, hotelType));
+            criteria.add(Restrictions.eq(ColumnNames.TOURS_FK_FOOD_COMPLEX + "." + ColumnNames.FOOD_COMPLEXES_ID, foodComplex));
+            List<Tour> list = criteria.list();
             return list;
         } catch (HibernateException e) {
             logger.writeLog("TourDAO  getToursByRequest error:" + e.getMessage());
